78a079888cc6f523db633fcb89b14d0f95a4c324
chenyong-ng <chenyong.ng@thermofisher.com>
2022-07-22 11:11:41 +0800

    new file:   Modules/MainFunction.ps1 	deleted:    Modules/MainFunction.psi

---
+++ Modules/MainFunction.ps1
@@ -0,0 +1,75 @@
+function Main {
+    If ($SerialRegMatch -eq $True) {
+        (Get-Process -Name PowerShell).MainWindowHandle | ForEach-Object { Set-WindowStyle MAXIMIZE $_ }
+        $StatusData_leaf = Get-ChildItem -Path "$path$name\" -I $StatusData  -R | Test-path -PathType Leaf
+        $GM_Analysis_leaf = Get-ChildItem -Path "$path$name\" -I $GM_Analysis -R | Test-path -PathType Leaf
+        Write-host "[Info   ]: RapidHIT Instrument $name detected, creating Server folder, Non-linearity Calibration and Waves place-holder file."
+        "[Info   ]: Force audio volume to 50%"
+        . U:\"RHID Troubleshooting"\set-volume.ps1
+        [audio]::Volume = 0.5
+        Set-MpPreference -DisableRealtimeMonitoring $true
+        if ([Bool] ($StatusData_leaf) -eq "True" ) {
+            "[Info   ]: Found $StatusData in these folders"
+            Get-ChildItem -Path "$path$name\*" -I $StatusData  -R | Format-table Directory -Autosize -HideTableHeaders -wrap
+        }
+        else { Write-host "[Info   ]: $StatusData not found, PDF not exported or no full run has been performed" -ForegroundColor yellow }
+        if ([Bool] ($GM_Analysis_leaf) -eq "True" ) {
+            "[Info   ]: Found $GM_Analysis in these folders"
+            Get-ChildItem -Path "$path$name\*" -I $GM_Analysis -R | Format-table Directory -Autosize -HideTableHeaders -wrap
+        }
+        else { Write-host "[Info   ]: $GM_Analysis not found or no full run has been performed" -ForegroundColor yellow }
+        if ($internal -eq $True) {
+            Write-host "[Info   ]: U:\$name\Internal\ already exists in server, skipping"
+        }
+        elseif ($internal -eq $False) {
+            mkdir U:\"$name"\Internal\
+            Write-host "[Info   ]: Server path $internal sucessfully created."
+        }
+        Set-Location $result
+        if ($nlc -eq $False) {
+            New-Item -Path "Non-linearity Calibration $name.PNG" -ItemType File
+            Write-host "[Info   ]: Created placeholder file: Non-linearity Calibration $name.PNG"
+        }
+        elseif ($nlfs -eq '0') {
+            Write-host "[Warning]: Empty file $nl detected, reported as $nlfs KB" -ForegroundColor red
+        }
+        else {
+            Write-Host "[Info   ]: 'Non-linearity Calibration $name.PNG' already exists, skipping, File size is:" $nlfs KB
+        }
+        if ($waves -eq $False) {
+            New-Item -Path "Waves $name.PNG" -ItemType File
+            Write-host "[Info   ]: Created placeholder file: Waves $name.PNG"
+        }
+        elseif ($wvfs -eq '0') {
+            Write-host "[Warning]: Empty file $wv detected, reported as $wvfs KB" -ForegroundColor red
+        }
+        else {
+            Write-Host "[Info   ]: 'Waves $name.PNG' already exists, skipping, File size is:" $wvfs KB
+        }
+
+        if ($mcleaf -eq $False) {
+            MachineConfigXML > $rhid\$MachineConfig
+            Write-host "[Info   ]: '$MachineConfig' created"
+        }
+        else {
+            Write-Host "[Info   ]: '$MachineConfig' already exists, skipping"
+        }
+        if ($tc -eq $False) {
+            TC_verification > "TC_verification $name.TXT"
+            Write-host "[Info   ] : Created placeholder file: TC_verification $name.TXT"
+        }
+        else {
+            Write-Host "[Info   ]: 'TC_verification $name.TXT' already exists, skipping"
+            Get-Content "TC_verification $name.TXT"
+        }
+
+        $keypress = read-host "[Info   ]: Enter y to open Snipping tool and Waves for taking screenshot, Enter to skip"
+        "[Info   ]: Make sure AutoFAT is not running, as Waves will cause resource conflict"
+        if ($keypress -eq 'y') {
+            Start-Process -WindowStyle Minimized -FilePath notepad.exe "TC_verification $name.TXT"
+            Start-Process -WindowStyle Minimized -FilePath SnippingTool.exe
+            Start-Process -WindowStyle Minimized -FilePath C:\"Program Files (x86)\RGB Lasersystems"\Waves\Waves.exe
+            Start-Process -WindowStyle Minimized -FilePath D:\gui-sec\gui_sec_V1001_4_79.exe
+        }
+    } # Main function to check whether if it's RHID instrument or Workstation
+}
