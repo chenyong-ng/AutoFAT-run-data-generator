54f41d4d8f775d1b72ffd978391cfa564911c6f3
chenyong-ng <chenyong.ng@thermofisher.com>
2022-07-28 01:23:21 +0800

    modified:   Modules/mtss.ps1

--- Modules/mtss.ps1
+++ Modules/mtss.ps1
@@ -1,1 +1,16 @@
+# Heater tests
 Get-ChildItem "$serverdir" -I storyboard*.* -R | Select-String "Lysis Heater FAT"  | Select-Object -Last 1
+
+# SCI tests
+
+# Mezzboard Motor
+
+# Mezz sensors
+
+# RFIDs
+
+# Laser
+
+# Wet tests
+
+# Bolus tests
