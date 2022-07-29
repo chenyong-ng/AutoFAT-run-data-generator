a2e541e63bad7d1977381542e34a4294b75feb78
chenyong-ng <chenyong.ng@thermofisher.com>
2022-07-29 16:37:01 +0800

    modified:   Modules/mtss.ps1

--- Modules/mtss.ps1
+++ Modules/mtss.ps1
@@ -1,38 +1,39 @@
 $storyboard = Get-ChildItem "$serverdir" -I storyboard*.* -R 
 
 $storyboard | Select-String "Q-mini serial number" | Select-Object -Last 1
-$storyboard | Select-String "Main board firmware version" | Select-Object -Last 1
-$storyboard | Select-String "Mezz board firmware version" | Select-Object -Last 1
+[bool]($storyboard | Select-String "Main board firmware version" | select-string "1001.4.79" | Select-Object -Last 1)
+[bool]($storyboard | Select-String "Mezz board firmware version" | select-string "1001.4.79" | Select-Object -Last 1)
 
 # Heater tests
 $MTSS_Lysis_Heater_FAT = [bool]($storyboard | Select-String "Lysis Heater FAT" | select-string "PASS"  | Select-Object -Last 1)
-if ("$MTSS_Lysis_Heater_FAT" -eq "True")
-{ Write-Host "Lysis Heater FAT test PASSED" -ForegroundColor green}
-else { Write-Host "Lysis Heater FAT test FAILED" -ForegroundColor red}
-$storyboard | Select-String "DN FAT"            | Select-Object -Last 1
-$storyboard | Select-String "PCR FAT"           | Select-Object -Last 1 
-$storyboard | Select-String "Optics Heater FAT" | Select-Object -Last 1
+if ("$MTSS_Lysis_Heater_FAT" -eq "True") {
+    Write-Host "Lysis Heater FAT test PASSED" -ForegroundColor green }
+else {
+    Write-Host "Lysis Heater FAT test FAILED" -ForegroundColor red }
+
+[bool]($storyboard | Select-String "DN FAT"            | select-string "PASS"  | Select-Object -Last 1)
+[bool]($storyboard | Select-String "PCR FAT"           | select-string "PASS"  | Select-Object -Last 1)
+[bool]($storyboard | Select-String "Optics Heater FAT" | select-string "PASS"  | Select-Object -Last 1)
 
 # Mainboard tests
-$storyboard | Select-String "Gel Cooling FAT"   | Select-Object -Last 1
-$storyboard | Select-String "Ambient FAT"       | Select-Object -Last 1
-
+[bool]($storyboard | Select-String "Gel Cooling FAT" | select-string "PASS"  | Select-Object -Last 1)
+[bool]($storyboard | Select-String "Ambient FAT"     | select-string "PASS"  | Select-Object -Last 1)
 # SCI tests
-$storyboard | Select-String "CAM FAT"             | Select-Object -Last 1
-$storyboard | Select-String "SCI Insertion FAT" | Select-Object -Last 1
-$storyboard | Select-String "FRONT END FAT"                  | Select-Object -Last 1
-$storyboard | Select-String "Bring Up: FE Motor Calibration" | Select-Object -Last 1
-$storyboard | Select-String "Bring Up: FE Motor Test"        | Select-Object -Last 1
-$storyboard | Select-String "Bring Up: Homing Erro r Test"    | Select-Object -Last 1
-$storyboard | Select-String "Bring Up: FL Homing Error w/CAM Test" | Select-Object -Last 1
-$storyboard | Select-String "SCI Antenna Test"               | Select-Object -Last 1
+[bool]($storyboard | Select-String "CAM FAT"           | select-string "PASS"  | Select-Object -Last 1)
+[bool]($storyboard | Select-String "SCI Insertion FAT" | select-string "PASS"  | Select-Object -Last 1)
+[bool]($storyboard | Select-String "FRONT END FAT"                  | select-string "PASS"  | Select-Object -Last 1)
+[bool]($storyboard | Select-String "Bring Up: FE Motor Calibration" | select-string "PASS"  | Select-Object -Last 1)
+[bool]($storyboard | Select-String "Bring Up: FE Motor Test"        | select-string "PASS"  | Select-Object -Last 1)
+[bool]($storyboard | Select-String "Bring Up: Homing Erro r Test"   | select-string "PASS"  | Select-Object -Last 1)
+[bool]($storyboard | Select-String "Bring Up: FL Homing Error w/CAM Test" | select-string "PASS"  | Select-Object -Last 1)
+[bool]($storyboard | Select-String "SCI Antenna Test"               | select-string "PASS"  | Select-Object -Last 1)
 
 # Mezzboard Motor
-$storyboard | Select-String "MEZZ test" | Select-Object -Last 1
-$storyboard | Select-String "LP FAT" | Select-Object -Last 1
-$storyboard | Select-String "HP FAT" | Select-Object -Last 1
-$storyboard | Select-String "Anode Motor FAT" | Select-Object -Last 1
-$storyboard | Select-String "BEC Interlock FAT" | Select-Object -Last 1
+[bool]($storyboard | Select-String "MEZZ test" | select-string "PASS"  | Select-Object -Last 1)
+[bool]($storyboard | Select-String "LP FAT" | select-string "PASS"  | Select-Object -Last 1)
+[bool]($storyboard | Select-String "HP FAT" | select-string "PASS"  | Select-Object -Last 1)
+[bool]($storyboard | Select-String "Anode Motor FAT" | select-string "PASS"  | Select-Object -Last 1)
+[bool]($storyboard | Select-String "BEC Interlock FAT" | select-string "PASS"  | Select-Object -Last 1)
 $storyboard | Select-String "Bring Up: Gel Antenna" | Select-Object -Last 1
 $storyboard | Select-String "Syringe Stallout FAT" | Select-Object -Last 1
 $storyboard | Select-String "Mezzboard FAT" | Select-Object -Last 1
