4201fba79e82c9a436d7a04462d8557b0dc44f1c
chenyong-ng <chenyong.ng@thermofisher.com>
2022-08-03 14:54:20 +0800

    modified:   Modules/mtss.ps1

--- Modules/mtss.ps1
+++ Modules/mtss.ps1
@@ -130,7 +130,7 @@ else {
 ($storyboard | Select-String "Bring Up: Verify Raman" | select-string "PASS" | Select-Object -Last 1).line.split(",")| Select-Object -Last 1
 
 $MTSS_Bolus = Get-ChildItem "$serverdir\*Bolus Delivery Test*"  -I  storyboard*.* -R | Select-String "Bolus Devliery Test" | select-string "PASS"
-# $MTSS_Bolus[2,3,4,5,6,7,8,9,0,1]
+# $MTSS_Bolus[2,3,4,5,6,7,8,9,0,1] (Get-ChildItem "$serverdir\*Bolus Delivery Test*"  -I  storyboard*.* -R |  select-string "Timing" | Select-Object -Last 1).line.split(",") | select-object -last 1
 Write-host Passed Bolus test count: $MTSS_Bolus.count
 
 # Print RFID of BEC, Sample Cartridge. separate mtss test with prefix such sd [HEATER], [SCI] etc., add history and test count, apply no filter.
