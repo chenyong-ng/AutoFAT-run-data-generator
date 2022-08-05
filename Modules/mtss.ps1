ee0b823b190e8de8b26d46e68ed1fa5ccc69c64a
chenyong-ng <chenyong.ng@thermofisher.com>
2022-08-05 10:32:31 +0800

    modified:   Modules/mtss.ps1 	modified:   automation.ps1

--- Modules/mtss.ps1
+++ Modules/mtss.ps1
@@ -103,12 +103,10 @@ else {
 ($storyboard | Select-String "MEZZ test" | select-string "PASS" | Select-Object -Last 1).line.split(",")| Select-Object -Last 1
 ($storyboard | Select-String "LP FAT"    | select-string "PASS" | Select-Object -Last 1).line.split(",")| Select-Object -Last 1
 ($storyboard | Select-String "HP FAT"    | select-string "PASS" | Select-Object -Last 1).line.split(",")| Select-Object -Last 1
-IF (($storyboard | Select-String "Anode Motor FAT").count -ne ("")) {
-$MTSS_Anode_Motor_FAT = ($storyboard | Select-String "Anode Motor FAT" | Select-Object -Last 1).line.split(",") | Select-Object -Last 1}
-
-if ($MTSS_Anode_Motor_FAT.count -eq "") {
+IF (($storyboard | Select-String "Anode Motor FAT").count -eq ("0")) {
     Write-Host "Anode Motor FAT test: N/A"    -ForegroundColor Yellow }
-elseif ([bool] ($MTSS_Anode_Motor_FAT | Select-String "Pass") -eq "True") {
+elseif ([bool]($storyboard | Select-String "Anode Motor FAT") -eq ("True")) {
+($storyboard | Select-String "Anode Motor FAT" | Select-Object -Last 1).line.split(",") | Select-Object -Last 1
     Write-Host "[Mezzplate] Anode Motor FAT test: PASSED" -ForegroundColor Green }
 else {
     Write-Host "[Mezzplate] Anode Motor FAT test: FAILED" -ForegroundColor Red    }
