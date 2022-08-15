f31a4eb7f676f91d6b96684c83946c3d704e7a3b
chenyong-ng <chenyong.ng@thermofisher.com>
2022-08-15 22:40:54 +0800

    modified:   Modules/rhid.ps1

--- Modules/rhid.ps1
+++ Modules/rhid.ps1
@@ -187,8 +187,8 @@ Write-Host "[ Humi Sensor] : Run end Humidity reading in % : $RHID_Hum_Rdr" -For
 
 # GM_ILS_Score_1,98
 # GM_ILS_Score_1_Name, Trace__Ladder.fsa
-$GM_ILS_Score = (Get-ChildItem -Exclude "Internal" | Get-ChildItem -I RunSummary.csv -R | select-string "GM_ILS_Score_2,").Line.TrimStart() 
-$GM_ILS_Score_Name = (Get-ChildItem -Exclude "Internal" | Get-ChildItem  -I RunSummary.csv -R | select-string "GM_ILS_Score_2_Name,").line.TrimStart()  
+$GM_ILS_Score = (Get-ChildItem -Exclude "Internal" -path "$serverdir" | Get-ChildItem -I RunSummary.csv -R | select-string "GM_ILS_Score_2,").Line.TrimStart() 
+$GM_ILS_Score_Name = (Get-ChildItem -Exclude "Internal"-path "$serverdir" | Get-ChildItem  -I RunSummary.csv -R | select-string "GM_ILS_Score_2_Name,").line.TrimStart()  
 #Write-Host "$Full_Run_Str : $GM_ILS_Score_Name GeneMarker ISL Score :" $GM_ILS_Score -ForegroundColor Green
 $GM_ILS_Score
 $GM_ILS_Score_Name
@@ -204,7 +204,8 @@ if ([Bool] ($StatusData_leaf | Select-Object -First 1) -eq "True" ) {
     Write-host "$Full_Run_Str : $StatusData $File_not_Found" -ForegroundColor yellow }
 
 if ([Bool] ($GM_Analysis_leaf | Select-Object -First 1) -eq "True" ) {
-    $RHID_GM_Analysis = Get-ChildItem -Exclude "Internal" -path "$serverdir" | Get-ChildItem -I $GM_Analysis -R | Format-table Directory -Autosize -HideTableHeaders -wrap
+    Set-Location $serverdir
+    $RHID_GM_Analysis = Get-ChildItem -Exclude "Internal" | Get-ChildItem -I $GM_Analysis | Format-table Directory -Autosize -HideTableHeaders -wrap
     Write-Host "$Full_Run_Str : $GM_Analysis $File_found" -ForegroundColor Green
     $RHID_GM_Analysis
 }
