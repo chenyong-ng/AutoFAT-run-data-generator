2af67d5f6cf02ba47431675839205a8e261148c9
chenyong-ng <chenyong.ng@thermofisher.com>
2022-08-18 19:00:07 +0800

    modified:   automation.bat

--- automation.bat
+++ automation.bat
@@ -1,11 +1,11 @@
 @Title RapidHIT ID Powershell Automation and Troubleshooting Tools
 @echo off
 :1
+clear
 If Exist "%~dp0\automation.ps1" (
 	powershell -Command "& {Set-ExecutionPolicy unrestricted -Scope CurrentUser}"
 	powershell -sta -ExecutionPolicy ByPass -File "%~dp0\automation.ps1"
-	clear-host
 )
 @echo Enter any key to Clear Console screen and check for new instrument.
-@Pause >:null
+Pause
 goto 1
