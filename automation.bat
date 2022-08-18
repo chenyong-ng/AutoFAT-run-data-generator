77882939fb09f4845d932d60871a9421618f9459
chenyong-ng <chenyong.ng@thermofisher.com>
2022-08-18 08:58:40 +0800

    modified:   automation.bat

--- automation.bat
+++ automation.bat
@@ -2,8 +2,8 @@
 @echo off
 :1
 If Exist "%~dp0\automation.ps1" (
-	Pwsh -Command "& {Set-ExecutionPolicy unrestricted -Scope CurrentUser}"
-	Pwsh.exe -sta -ExecutionPolicy ByPass -File "%~dp0\automation.ps1"
+	powershell -Command "& {Set-ExecutionPolicy unrestricted -Scope CurrentUser}"
+	powershell -sta -ExecutionPolicy ByPass -File "%~dp0\automation.ps1"
 	clear-host
 )
 @echo Enter any key to Clear Console screen and check for new instrument.
