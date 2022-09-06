7331217b60d55073beafc063f7786b842e06d206
chenyong-ng <chenyong.ng@thermofisher.com>
2022-09-06 14:58:59 +0800

    modified:   Modules/RHID_Report.ps1

--- Modules/RHID_Report.ps1
+++ Modules/RHID_Report.ps1
@@ -448,6 +448,7 @@ Write-Host "$USB_Temp : $USB_Temp_RD : $RHID_USB_Temp_Rdr" -ForegroundColor Cyan
 Write-Host "$USB_Humi : $USB_Humi_RD : $RHID_USB_Humi_Rdr" -ForegroundColor Cyan
 $Section_Separator
 
+# ignore folder with 0 size
 $Remote = "{0:N4} GB" -f ((Get-ChildItem -force "$Drive\$MachineName\Internal\"  -Recurse -ErrorAction SilentlyContinue | Measure-Object Length -sum ).sum / 1Gb)
 $Local = "{0:N4} GB" -f ((Get-ChildItem -force "E:\RapidHIT ID"             -Recurse -ErrorAction SilentlyContinue | Measure-Object Length -sum ).sum / 1Gb)
 $RemoteFileCount = (Get-ChildItem "$Drive\$MachineName\Internal\"  -Recurse -ErrorAction SilentlyContinue | Measure-Object).Count 
