c0ce8e272f4f1afd5497b4625b28bf0de9f6829d
chenyong-ng <chenyong.ng@thermofisher.com>
2023-03-05 22:36:15 +0800

    modified:   Modules/RHID_Troubleshooting.xml 	renamed:    Modules/automation.ps1 -> Modules/Script_Automation.ps1 	renamed:    automation.bat -> RHID_troubleshooter.bat

--- RHID_troubleshooter.bat
+++ RHID_troubleshooter.bat
@@ -4,7 +4,7 @@
 @REM This batch file invokes the powershell scripts under the modules foder with elevated privileges
 If Exist "%~dp0\Modules\automation.ps1" (
 	powershell -Command "& {Set-ExecutionPolicy unrestricted -Scope CurrentUser}"
-	powershell -sta -ExecutionPolicy ByPass -File "%~dp0\Modules\automation.ps1"
+	powershell -sta -ExecutionPolicy ByPass -File "%~dp0\Modules\Script_Automation.ps1"
 )
 @echo Enter any key to Clear Console screen and check for new instrument.
 Pause
