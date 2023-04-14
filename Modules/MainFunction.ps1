f7b281677258e663da075d30af272802d41109af
chenyong.ng <chenyong.ng@thermofisher.com>
2023-04-14 14:21:28 +0800

    modified:   Modules/MainFunction.ps1     optimized OS version and host name queries

--- Modules/MainFunction.ps1
+++ Modules/MainFunction.ps1
@@ -22,10 +22,9 @@ $RHID_FP_Sensor = (Get-PnpDevice -PresentOnly | Where-Object { $_.InstanceId -ma
 $RHID_USB_HD_Camera = (Get-PnpDevice -PresentOnly | Where-Object { $_.InstanceId -match '^USB' } | Select-String "HD USB Camera" )
 If ([Bool]$RHID_FP_Sensor -eq "True") {"$FP : $FP_Sensor_Str : Present" } else { "$FP : $FP_Sensor_Str : N/A" }
 If ([Bool]$RHID_USB_HD_Camera -eq "True") {"$HD_USB_CAM : $HD_USB_CAM_Str : Present"} else {"$HD_USB_CAM : $HD_USB_CAM_Str : N/A"}
-$OSQuery_Name = (systeminfo | select-string "OS name").line.split(":").TrimStart() | select-object -last 1
-$Host_Query = (systeminfo | select-string "Host Name").line.split(":").TrimStart() | select-object -last 1
-"$System : $Operating_System : $OSQuery_Name"
-"$System : $Host_Name : $Host_Query"
+$SystemQuery = ((systeminfo | select-string "OS name", "Host Name").line.split(":").TrimStart())[1,-1]
+"$System : $Operating_System :", $SystemQuery[1]
+"$System : $Host_Name :", $SystemQuery[0]
     $Win10patch_leaf = Test-Path -Path "$Win110Patch_RegKey" 
     if ($Win10patch_leaf -eq "True") {
         $Win10patch = Get-ItemPropertyValue "$Win110Patch_RegKey" 'DisplayName'
