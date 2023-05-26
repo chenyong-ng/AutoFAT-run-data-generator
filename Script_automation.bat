c66c708109124495017d1172e993cb682f2ab086
chenyong-ng <chenyong.ng@thermofisher.com>
2023-05-27 02:05:12 +0800

    modified:   Script_automation.bat

--- Script_automation.bat
+++ Script_automation.bat
@@ -1,18 +1,26 @@
 @Title [%computername%] RapidHIT ID Powershell Automation and Troubleshooting Tools
 @echo off
+rem Temporary workaround to avoid powershell missing connsole capture, until XML export is implemented
 :0
-set RHID_Workstation=SGSI11-59FKK13
-set RHID_Devel=SGSI11-B6BZP73
 set InstFolder=U:\%computername%\Internal\"RapidHIT ID\Results\Data %computername%"
 set InstResultTXT=U:\%computername%\Internal\"RapidHIT ID\Results\%computername% Result.TXT"
-if %computername% == (%RHID_Devel% OR %RHID_Workstation%) set InstResultTXT=NUL
 
+set RHID_Workstation=SGSI11-59FKK13
+rem set RHID_Devel=SGSI11-B6BZP73
+set RHID_Devel=LENOVO-R76800HS
+if "%computername%" == "%RHID_Devel%" (goto 1)
 If Exist "%~dp0\Modules\Report_Automation.ps1" (
 	if not exist %InstFolder% mkdir %InstFolder%
 	powershell -Command "& {Set-ExecutionPolicy unrestricted -Scope CurrentUser}"
 	powershell -sta -ExecutionPolicy ByPass -File "%~dp0\Modules\Report_Automation.ps1" > %InstResultTXT%
+	goto 3
 )
-:2
+:1
+If Exist "%~dp0\Modules\Report_Automation.ps1" (
+	powershell -Command "& {Set-ExecutionPolicy unrestricted -Scope CurrentUser}"
+	powershell -sta -ExecutionPolicy ByPass -File "%~dp0\Modules\Report_Automation.ps1"
+)
+:3
 @echo Press any key to Clear Console screen and refresh test result.
 Pause
 goto 0
