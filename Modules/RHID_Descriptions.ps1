c89bd34c2514a07db6760925fb9010272d6c5ede
chenyong-ng <chenyong.ng@thermofisher.com>
2023-06-23 17:21:33 +0800

    modified:   Modules/RHID_Descriptions.ps1

--- Modules/RHID_Descriptions.ps1
+++ Modules/RHID_Descriptions.ps1
@@ -3,6 +3,7 @@ $Desc_TC            = "$Desc : SCI Thermocycler Calibration data, NaN means not 
 $Desc_MachineConfig = "$Desc : Info extracted from MachineCofig.xml file, Essential files"
 $Desc_Firmware      = "$Desc : Win10 firmware 1001.4.79 is the Production version"
 $Desc_HIDAutoLite   = "$Desc : TestPrep installed HIDAutoLite only valid for 35 days, Data extracted from Execution.Log"
+# add location info
 
 <#
 Provides more detailed test results
@@ -19,7 +20,7 @@ $RHID_Lysis2_Temp_Avg = (($storyboard | Select-String "Lysis2 Temp Average =" | 
 $RHID_Lysis2_Temp_SD = (($storyboard | Select-String "Lysis2 Temp SD =" | Select-String "(< 0.25C)")[-1].line.split(",").TrimStart())[-1]
 $RHID_Lysis2_PWM_Avg = (($storyboard | Select-String "Lysis2 PWM Average =" | Select-String "(0/8000)")[-1].line.split(",").TrimStart())[-1]
 $RHID_Lysis2_PWM_SD = (($storyboard | Select-String "Lysis2 PWM SD =" | Select-String "(< 1000)")[-1].line.split(",").TrimStart())[-1]
-
+#change to details
 "$Desc : $RHID_Lysis1_Ramp"
 "$Desc : $RHID_Lysis1_Temp_Avg"
 "$Desc : $RHID_Lysis1_Temp_SD"
