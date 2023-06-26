0f558d081ecead234bbe3bdd9db33d972e9aaac4
chenyong-ng <chenyong.ng@thermofisher.com>
2023-06-27 01:13:43 +0800

    modified:   Modules/CheckSum.ps1 	modified:   Script_automation.bat

--- Modules/CheckSum.ps1
+++ Modules/CheckSum.ps1
@@ -1,52 +1,54 @@
 
-$TestPath_CheckSum          = Test-Path -Path $PSScriptRoot\CheckSum.ps1 -PathType Leaf
-$TestPath_AdapterTypes      = Test-Path -Path $PSScriptRoot\AdapterTypes.ps1 -PathType Leaf
-$TestPath_Branch            = Test-Path -Path $PSScriptRoot\Branch.ps1 -PathType Leaf
-$TestPath_Info_Screens      = Test-Path -Path $PSScriptRoot\Info_Screens.ps1 -PathType Leaf
-$TestPath_Report_Automation = Test-Path -Path $PSScriptRoot\Report_Automation.ps1 -PathType Leaf
-$TestPath_RHID_CoverOnTest  = Test-Path -Path $PSScriptRoot\RHID_CoverOnTest.ps1 -PathType Leaf
-$TestPath_RHID_Descriptions = Test-Path -Path $PSScriptRoot\RHID_Descriptions.ps1 -PathType Leaf
-$TestPath_RHID_DryTest      = Test-Path -Path $PSScriptRoot\RHID_DryTest.ps1 -PathType Leaf
-$TestPath_RHID_DryTestHeader= Test-Path -Path $PSScriptRoot\RHID_DryTestHeader.ps1 -PathType Leaf
-$TestPath_RHID_Hardware     = Test-Path -Path $PSScriptRoot\RHID_Hardware.ps1 -PathType Leaf
-$TestPath_RHID_MainFunction = Test-Path -Path $PSScriptRoot\RHID_MainFunction.ps1 -PathType Leaf
-$TestPath_RHID_Report       = Test-Path -Path $PSScriptRoot\RHID_Report.ps1 -PathType Leaf
-$TestPath_RHID_ShipPrep     = Test-Path -Path $PSScriptRoot\RHID_ShipPrep.ps1 -PathType Leaf
-$TestPath_RHID_Str          = Test-Path -Path $PSScriptRoot\RHID_Str.ps1 -PathType Leaf
-$TestPath_RHID_WetTest      = Test-Path -Path $PSScriptRoot\RHID_WetTest.ps1 -PathType Leaf
-$TestPath_RHID_XmlWriter    = Test-Path -Path $PSScriptRoot\RHID_XmlWriter.ps1 -PathType Leaf
-$TestPath_RunSummaryCSV     = Test-Path -Path $PSScriptRoot\RunSummaryCSV.ps1 -PathType Leaf
-$TestPath_SetScreenResolutionEx = Test-Path -Path $PSScriptRoot\Set-ScreenResolutionEx.ps1 -PathType Leaf
-$TestPath_setvolume         = Test-Path -Path $PSScriptRoot\set-volume.ps1 -PathType Leaf
-$TestPath_SetWindowStyle    = Test-Path -Path $PSScriptRoot\Set-WindowStyle.ps1 -PathType Leaf
-$TestPath_TC_VerificationTXT = Test-Path -Path $PSScriptRoot\TC_VerificationTXT.ps1 -PathType Leaf
-$TestPath_VerboseMode       = Test-Path -Path $PSScriptRoot\VerboseMode.ps1 -PathType Leaf
-$TestPath_XML_and_Config    = Test-Path -Path $PSScriptRoot\XML_and_Config.ps1 -PathType Leaf
-
 $ScriptPreCheck =
-$TestPath_CheckSum,
-$TestPath_AdapterTypes,
-$TestPath_Branch,
-$TestPath_Info_Screens,
-$TestPath_Report_Automation,
-$TestPath_RHID_CoverOnTest,
-$TestPath_RHID_Descriptions,
-$TestPath_RHID_DryTest,
-$TestPath_RHID_DryTestHeader,
-$TestPath_RHID_Hardware,
-$TestPath_RHID_MainFunction,
-$TestPath_RHID_Report,
-$TestPath_RHID_ShipPrep,
-$TestPath_RHID_Str,
-$TestPath_RHID_WetTest,
-$TestPath_RHID_XmlWriter,
-$TestPath_RunSummaryCSV,
-$TestPath_SetScreenResolutionEx,
-$TestPath_setvolume,
-$TestPath_SetWindowStyle,
-$TestPath_TC_VerificationTXT,
-$TestPath_VerboseMode,
-$TestPath_XML_and_Config
+Test-Path -Path $PSScriptRoot\CheckSum.ps1 ,
+$PSScriptRoot\AdapterTypes.ps1 ,
+$PSScriptRoot\Branch.ps1 ,
+$PSScriptRoot\Info_Screens.ps1 ,
+$PSScriptRoot\Report_Automation.ps1 ,
+$PSScriptRoot\RHID_CoverOnTest.ps1 ,
+$PSScriptRoot\RHID_Descriptions.ps1 ,
+$PSScriptRoot\RHID_DryTest.ps1 ,
+$PSScriptRoot\RHID_DryTestHeader.ps1 ,
+$PSScriptRoot\RHID_Hardware.ps1 ,
+$PSScriptRoot\RHID_MainFunction.ps1 ,
+$PSScriptRoot\RHID_Report.ps1 ,
+$PSScriptRoot\RHID_ShipPrep.ps1 ,
+$PSScriptRoot\RHID_Str.ps1 ,
+$PSScriptRoot\RHID_WetTest.ps1 ,
+$PSScriptRoot\RHID_XmlWriter.ps1 ,
+$PSScriptRoot\RunSummaryCSV.ps1 ,
+$PSScriptRoot\Set-ScreenResolutionEx.ps1 ,
+$PSScriptRoot\set-volume.ps1 ,
+$PSScriptRoot\Set-WindowStyle.ps1 ,
+$PSScriptRoot\TC_VerificationTXT.ps1 ,
+$PSScriptRoot\VerboseMode.ps1 ,
+$PSScriptRoot\XML_and_Config.ps1 -PathType Leaf
+
+
+(Get-FileHash $PSScriptRoot\..\Script_automation.bat ,
+$PSScriptRoot\AdapterTypes.ps1 ,
+$PSScriptRoot\Branch.ps1 ,
+$PSScriptRoot\CheckSum.ps1 ,
+$PSScriptRoot\Info_Screens.ps1 ,
+$PSScriptRoot\Report_Automation.ps1 ,
+$PSScriptRoot\RHID_CoverOnTest.ps1 ,
+$PSScriptRoot\RHID_Descriptions.ps1 ,
+$PSScriptRoot\RHID_DryTest.ps1 ,
+$PSScriptRoot\RHID_DryTestHeader.ps1 ,
+$PSScriptRoot\RHID_Hardware.ps1 ,
+$PSScriptRoot\RHID_MainFunction.ps1 ,
+$PSScriptRoot\RHID_Report.ps1 ,
+$PSScriptRoot\RHID_ShipPrep.ps1 ,
+$PSScriptRoot\RHID_Str.ps1 ,
+$PSScriptRoot\RHID_WetTest.ps1 ,
+$PSScriptRoot\RHID_XmlWriter.ps1 ,
+$PSScriptRoot\RunSummaryCSV.ps1 ,
+$PSScriptRoot\Set-ScreenResolutionEx.ps1 ,
+$PSScriptRoot\set-volume.ps1 ,
+$PSScriptRoot\Set-WindowStyle.ps1 ,
+$PSScriptRoot\TC_VerificationTXT.ps1 ,
+$PSScriptRoot\XML_and_Config.ps1 -Algorithm SHA256).hash
+# Get-FileHash is bugged in Powershell 5.1, only hashing and compare hastable in Powershell 7
 
 $ScriptPreCheckCounter = ($ScriptPreCheck | select-string "true").count
 
--- Script_automation.bat
+++ Script_automation.bat
@@ -2,7 +2,7 @@
 @echo off
 :1
 If Exist "%~dp0\Modules\Report_Automation.ps1" (
-	powershell -sta -ExecutionPolicy ByPass -File "%~dp0\Modules\Report_Automation.ps1"
+	Powershell -sta -ExecutionPolicy ByPass -File "%~dp0\Modules\Report_Automation.ps1"
 )
 @echo [Batch Script] : Press any key to Clear Console screen and refresh test result.
 @Pause>:nul
