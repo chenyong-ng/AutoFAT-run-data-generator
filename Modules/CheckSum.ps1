a7271d3c6d966582c3451932e73bb19fb42250f1
chenyong-ng <chenyong.ng@thermofisher.com>
2023-06-28 16:09:48 +0800

    modified:   Modules/CheckSum.ps1

--- Modules/CheckSum.ps1
+++ Modules/CheckSum.ps1
@@ -3,76 +3,80 @@ $File_Script_automation = "$PSScriptRoot\..\Script_automation.bat"
 $File_CheckSum          = "$PSScriptRoot\CheckSum.ps1"
 $File_AdapterTypes      = "$PSScriptRoot\AdapterTypes.ps1"
 $File_Branch            = "$PSScriptRoot\Branch.ps1"
-$File_Info_Screens      = $PSScriptRoot\Info_Screens.ps1
-$File_Report_Automation = $PSScriptRoot\Report_Automation.ps1
-$File_RHID_CoverOnTest  = $PSScriptRoot\RHID_CoverOnTest.ps1 ,
-$File_RHID_Descriptions = $PSScriptRoot\RHID_Descriptions.ps1
-$File_RHID_DryTest      = $PSScriptRoot\RHID_DryTest.ps1
-$File_RHID_DryTestHeader = $PSScriptRoot\RHID_DryTestHeader.ps1
-$File_RHID_Hardware     = $PSScriptRoot\RHID_Hardware.ps1
-$File_RHID_MainFunction = $PSScriptRoot\RHID_MainFunction.ps1
-$File_RHID_Report       = $PSScriptRoot\RHID_Report.ps1
-$File_RHID_ShipPrep     = $PSScriptRoot\RHID_ShipPrep.ps1
-$File_RHID_Str          = $PSScriptRoot\RHID_Str.ps1
-$File_RHID_WetTest      = $PSScriptRoot\RHID_WetTest.ps1
-$File_RHID_XmlWriter    = $PSScriptRoot\RHID_XmlWriter.ps1
-$File_RunSummaryCSV     = $PSScriptRoot\RunSummaryCSV.ps1
-$File_Set-ScreenResolutionEx = $PSScriptRoot\Set-ScreenResolutionEx.ps1
-$File_set-volume        = $PSScriptRoot\set-volume.ps1
-$File_Set-WindowStyle   = $PSScriptRoot\Set-WindowStyle.ps1
-$File_TC_VerificationTXT= $PSScriptRoot\TC_VerificationTXT.ps1
-$File_VerboseMode       = $PSScriptRoot\VerboseMode.ps1
-$File_XML_and_Config    = $PSScriptRoot\XML_and_Config.ps1
+$File_Info_Screens      = "$PSScriptRoot\Info_Screens.ps1"
+$File_Report_Automation = "$PSScriptRoot\Report_Automation.ps1"
+$File_RHID_CoverOnTest  = "$PSScriptRoot\RHID_CoverOnTest.ps1"
+$File_RHID_Descriptions = "$PSScriptRoot\RHID_Descriptions.ps1"
+$File_RHID_DryTest      = "$PSScriptRoot\RHID_DryTest.ps1"
+$File_RHID_DryTestHeader = "$PSScriptRoot\RHID_DryTestHeader.ps1"
+$File_RHID_Hardware     = "$PSScriptRoot\RHID_Hardware.ps1"
+$File_RHID_MainFunction = "$PSScriptRoot\RHID_MainFunction.ps1"
+$File_RHID_Report       = "$PSScriptRoot\RHID_Report.ps1"
+$File_RHID_ShipPrep     = "$PSScriptRoot\RHID_ShipPrep.ps1"
+$File_RHID_Str          = "$PSScriptRoot\RHID_Str.ps1"
+$File_RHID_WetTest      = "$PSScriptRoot\RHID_WetTest.ps1"
+$File_RHID_XmlWriter    = "$PSScriptRoot\RHID_XmlWriter.ps1"
+$File_RunSummaryCSV     = "$PSScriptRoot\RunSummaryCSV.ps1"
+$File_SetScreenResolutionEx = "$PSScriptRoot\Set-ScreenResolutionEx.ps1"
+$File_setvolume         = "$PSScriptRoot\set-volume.ps1"
+$File_SetWindowStyle    = "$PSScriptRoot\Set-WindowStyle.ps1"
+$File_TC_VerificationTXT= "$PSScriptRoot\TC_VerificationTXT.ps1"
+$File_VerboseMode       = "$PSScriptRoot\VerboseMode.ps1"
+$File_XML_and_Config    = "$PSScriptRoot\XML_and_Config.ps1"
 
 $ScriptPreCheck =
-Test-Path -Path $PSScriptRoot\CheckSum.ps1 ,
-$PSScriptRoot\AdapterTypes.ps1 ,
-$PSScriptRoot\Branch.ps1 ,
-$PSScriptRoot\Info_Screens.ps1 ,
-$PSScriptRoot\Report_Automation.ps1 ,
-$PSScriptRoot\RHID_CoverOnTest.ps1 ,
-$PSScriptRoot\RHID_Descriptions.ps1 ,
-$PSScriptRoot\RHID_DryTest.ps1 ,
-$PSScriptRoot\RHID_DryTestHeader.ps1 ,
-$PSScriptRoot\RHID_Hardware.ps1 ,
-$PSScriptRoot\RHID_MainFunction.ps1 ,
-$PSScriptRoot\RHID_Report.ps1 ,
-$PSScriptRoot\RHID_ShipPrep.ps1 ,
-$PSScriptRoot\RHID_Str.ps1 ,
-$PSScriptRoot\RHID_WetTest.ps1 ,
-$PSScriptRoot\RHID_XmlWriter.ps1 ,
-$PSScriptRoot\RunSummaryCSV.ps1 ,
-$PSScriptRoot\Set-ScreenResolutionEx.ps1 ,
-$PSScriptRoot\set-volume.ps1 ,
-$PSScriptRoot\Set-WindowStyle.ps1 ,
-$PSScriptRoot\TC_VerificationTXT.ps1 ,
-$PSScriptRoot\VerboseMode.ps1 ,
-$PSScriptRoot\XML_and_Config.ps1 -PathType Leaf
+Test-Path -Path
+$File_Script_automation 
+$File_CheckSum          
+$File_AdapterTypes      
+$File_Branch            
+$File_Info_Screens      
+$File_Report_Automation 
+$File_RHID_CoverOnTest  
+$File_RHID_Descriptions 
+$File_RHID_DryTest      
+$File_RHID_DryTestHeader
+$File_RHID_Hardware     
+$File_RHID_MainFunction 
+$File_RHID_Report       
+$File_RHID_ShipPrep     
+$File_RHID_Str          
+$File_RHID_WetTest      
+$File_RHID_XmlWriter    
+$File_RunSummaryCSV     
+$File_SetScreenResolutio
+$File_setvolume         
+$File_SetWindowStyle    
+$File_TC_VerificationTXT
+$File_VerboseMode       
+$File_XML_and_Config -PathType Leaf
 
 
-(Get-FileHash $PSScriptRoot\..\Script_automation.bat ,
-$PSScriptRoot\AdapterTypes.ps1 ,
-$PSScriptRoot\Branch.ps1 ,
-$PSScriptRoot\CheckSum.ps1 ,
-$PSScriptRoot\Info_Screens.ps1 ,
-$PSScriptRoot\Report_Automation.ps1 ,
-$PSScriptRoot\RHID_CoverOnTest.ps1 ,
-$PSScriptRoot\RHID_Descriptions.ps1 ,
-$PSScriptRoot\RHID_DryTest.ps1 ,
-$PSScriptRoot\RHID_DryTestHeader.ps1 ,
-$PSScriptRoot\RHID_Hardware.ps1 ,
-$PSScriptRoot\RHID_MainFunction.ps1 ,
-$PSScriptRoot\RHID_Report.ps1 ,
-$PSScriptRoot\RHID_ShipPrep.ps1 ,
-$PSScriptRoot\RHID_Str.ps1 ,
-$PSScriptRoot\RHID_WetTest.ps1 ,
-$PSScriptRoot\RHID_XmlWriter.ps1 ,
-$PSScriptRoot\RunSummaryCSV.ps1 ,
-$PSScriptRoot\Set-ScreenResolutionEx.ps1 ,
-$PSScriptRoot\set-volume.ps1 ,
-$PSScriptRoot\Set-WindowStyle.ps1 ,
-$PSScriptRoot\TC_VerificationTXT.ps1 ,
-$PSScriptRoot\XML_and_Config.ps1 -Algorithm SHA256).hash
+(Get-FileHash $File_Script_automation 
+$File_CheckSum          
+$File_AdapterTypes      
+$File_Branch            
+$File_Info_Screens      
+$File_Report_Automation 
+$File_RHID_CoverOnTest  
+$File_RHID_Descriptions 
+$File_RHID_DryTest      
+$File_RHID_DryTestHeader
+$File_RHID_Hardware     
+$File_RHID_MainFunction 
+$File_RHID_Report       
+$File_RHID_ShipPrep     
+$File_RHID_Str          
+$File_RHID_WetTest      
+$File_RHID_XmlWriter    
+$File_RunSummaryCSV     
+$File_SetScreenResolutio
+$File_setvolume         
+$File_SetWindowStyle    
+$File_TC_VerificationTXT
+$File_VerboseMode       
+$File_XML_and_Config -Algorithm SHA256).hash
+
 # Get-FileHash is bugged in Powershell 5.1, only hashing and compare hastable in Powershell 7
 
 $ScriptPreCheckCounter = ($ScriptPreCheck | select-string "true").count
