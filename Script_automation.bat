2aeeb7de3a9238caeb401dd5802ef30ac4e898f2
chenyong-ng <chenyong.ng@thermofisher.com>
2023-07-03 01:18:40 +0800

    modified:   Script_automation.bat

--- Script_automation.bat
+++ Script_automation.bat
@@ -1,14 +1,12 @@
 @Title [%computername%] RapidHIT ID Powershell Automation and Troubleshooting Tools
 @echo off
 :1
-If %computername%==DESKTOP-FR43358 (
-	set Powershell_version=Pwsh.exe
-) else (
-	set Powershell_version=Powershell.exe )
-rem "DESKTOP-FR43358" "LENOVO-R76800HS"
+
+rem "DESKTOP-FR43358" "LENOVO-R76800HS" 	Powershell -mta -ExecutionPolicy ByPass -File "%~dp0\Modules\Report_Automation.ps1"
 pushd %~dp0
 If Exist "%~dp0\Modules\Report_Automation.ps1" (
-	%Powershell_version% -mta -ExecutionPolicy ByPass -File "%~dp0\Modules\Report_Automation.ps1"
+	Powershell -Command "& { if ($env:computername -notmatch {RHID-\d\d\d\d}) { pwsh.exe -mta -ExecutionPolicy ByPass -File "%~dp0\Modules\Report_Automation.ps1"} else { Powershell -mta -ExecutionPolicy ByPass -File "%~dp0\Modules\Report_Automation.ps1"}}"
+rem Workaround for different Powershell version in Workstation environment and the Instruments
 )
 @echo [Batch Script] : Press any key to Clear Console screen and refresh test result.
 @Pause>:nul
